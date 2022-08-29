8cae5c0db217541273728e9dd1dac4b15ca0cdaa
Rogier Borst <rogier.borst@paqt.com>
2022-08-29 14:05:31 +0200

    show validation errors

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -69,15 +69,4 @@ class PostController extends Controller
     {
         //
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Post $post)
-    {
-        //
-    }
 }
--- resources/assets/nuxt3/pages/posts/index.vue
+++ resources/assets/nuxt3/pages/posts/index.vue
@@ -1,8 +1,13 @@
 <template>
-    <div class="container py-8 divide-y space-y-8 divide-gray-300">
-        <div v-for="post in posts" class="flex justify-between">
-            <span class="font-bold">{{ post.title }}</span>
-            <span class="italic">{{ post.user.name }}</span>
+    <div>
+        <div class="mr-auto">
+            <NuxtLink to="/posts/create" class="bg-amber-300 shadow rounded px-3 py-2">+</NuxtLink>
+        </div>
+        <div class="container py-8 divide-y space-y-8 divide-gray-300">
+            <div v-for="post in posts" class="flex justify-between">
+                <span class="font-bold">{{ post.title }}</span>
+                <span class="italic">{{ post.user.name }}</span>
+            </div>
         </div>
     </div>
 </template>
